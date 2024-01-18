fde17698c08f62a31a2ef3d4bb965ffa98e02e25
정훈 <wjdgnsxhsl@naver.com>
2024-01-18 17:28:22 +0900

    fix blog contents

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "정훈",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profileImg.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Front-End Developer",
     bio: "개발을 위한 개발",
     email: "wjdgnsxhsl@naver.com",
