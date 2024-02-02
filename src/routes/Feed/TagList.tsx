66baf990c8cbd986072a06fafdea3732c7137287
정훈 <wjdgnsxhsl@naver.com>
2024-02-02 16:02:51 +0900

    edit MainTags cause of reactive

--- src/routes/Feed/TagList.tsx
+++ src/routes/Feed/TagList.tsx
@@ -11,7 +11,8 @@ const TagList: React.FC<Props> = () => {
   const currentTag = router.query.tag || undefined
   const data = useTagsQuery()
   const [dataKeys, setDataKeys] = useState<String[][]>([])
-  const mainTags = new Set<String>()
+  const [mainTags, setMainTags] = useState(new Map<String, String[]>())
+  // const mainTags = new Map<String, String[]>()
 
   const handleClickTag = (value: any) => {
     // delete
@@ -34,42 +35,49 @@ const TagList: React.FC<Props> = () => {
     }
   }
 
-  const tagContents = (mapValue: String[], i: number) => {
-    const stringValue: String = mapValue.join("::")
-    if (!mainTags.has(mapValue[1])) {
-      mainTags.add(mapValue[1])
-      return (
-        <div key={i}>
-          <span className="mainTags">{mapValue[1]}</span>
-          <a
-            data-active={stringValue === currentTag}
-            onClick={() => handleClickTag(stringValue)}
-          >
-            - {mapValue[2]}
-          </a>
-        </div>
-      )
-    } else {
-      return (
-        <a
-          key={i}
-          data-active={stringValue === currentTag}
-          onClick={() => handleClickTag(stringValue)}
-        >
-          - {mapValue[2]}
-        </a>
-      )
-    }
+  const tagContents = () => {
+    var mainTagIndex = 0
+    return (
+      <>
+        {Array.from(mainTags).map(([key, value]) => {
+          mainTagIndex++
+          return (
+            <li key={String(key)} className="mainTags">
+              <div>{key}</div>
+              {value.map((subTag, index) => {
+                const originTag: String =
+                  mainTagIndex + "::" + key + "::" + subTag
+                console.log(originTag)
+                return (
+                  <a
+                    key={index}
+                    data-active={originTag === currentTag}
+                    onClick={() => handleClickTag(originTag)}
+                  >
+                    - {subTag}
+                  </a>
+                )
+              })}
+            </li>
+          )
+        })}
+      </>
+    )
   }
 
   useEffect(() => {
-    const tempArray: String[][] = []
-    Object.keys(data).map((value) => {
-      const splitted: String[] = value.split("::")
-      if (splitted[2] === undefined || splitted[2].length <= 1) return
-      tempArray.push(splitted)
-    })
-    setDataKeys(tempArray.sort())
+    const tempMainTags = new Map<String, String[]>()
+    Object.keys(data)
+      .sort() //순서대로 정렬
+      .map((value) => {
+        const splitted: String[] = value.split("::") //[0,SW공부,CS], [0, SW공부, JavaScript], ...
+        if (splitted[2] === undefined || splitted[2].length <= 1) return
+        const currentSubTagsArray: String[] =
+          tempMainTags.get(splitted[1]) || []
+        currentSubTagsArray.push(splitted[2])
+        tempMainTags.set(splitted[1], currentSubTagsArray)
+      })
+    setMainTags(tempMainTags)
   }, [])
 
   return (
@@ -77,11 +85,7 @@ const TagList: React.FC<Props> = () => {
       <div className="top">
         <Emoji>🏷️</Emoji> 태그
       </div>
-      <div className="list">
-        {dataKeys.map((mapValue, index) => {
-          return tagContents(mapValue, index)
-        })}
-      </div>
+      <div className="list">{tagContents()}</div>
     </StyledWrapper>
   )
 }
@@ -101,62 +105,77 @@ const StyledWrapper = styled.div`
 
   .list {
     display: flex;
-    margin-bottom: 1.5rem;
-    gap: 0.25rem;
+    margin-bottom: 1rem;
     overflow: scroll;
-
     scrollbar-width: none;
+    gap : 1.5rem;
     -ms-overflow-style: none;
     ::-webkit-scrollbar {
       width: 0;
       height: 0;
     }
-
+    
     @media (min-width: 1024px) {
       display: block;
     }
-
-    .mainTags {
-      display: block;
-      padding: 0.5rem;
-      padding-left: 0.3rem;
-      padding-right: 0.3rem;
-      margin-top:0.3rem
-      margin-bottom: 0.3rem;
-      border-radius: 0.5rem;
-      font-size: 0.975rem;
-      line-height:1rem;
-      font-weight : 600;
-      color: ${({ theme }) => theme.colors.gray11};
-      flex-shrink:0;
-      cursor:default;
-    }
     
-    a {
-      display: block;
-      padding: 0.25rem;
-      padding-left: 1rem;
-      padding-right: 1rem;
-      margin-top: 0.25rem;
-      margin-bottom: 0.25rem;
-      border-radius: 0.75rem;
-      font-size: 0.875rem;
-      line-height: 1.25rem;
-      color: ${({ theme }) => theme.colors.gray10};
-      flex-shrink: 0;
-      cursor: pointer;
-
-      :hover {
-        background-color: ${({ theme }) => theme.colors.gray4};
+    .mainTags {
+      @media (min-width: 1024px) {
+        display: block;
       }
-      &[data-active="true"] {
-        color: ${({ theme }) => theme.colors.gray12};
-        background-color: ${({ theme }) => theme.colors.gray4};
+      @media(max-width:1023px){
+        div{
+          display : flex;
+          justify-content : center;
+          align-items : center;
+        }
+      }
+      display: flex;
+      gap: 0.25rem;
+      margin-bottom : 1rem;
+      flex-shrink : 0;
+      div{
+        padding: 0.5rem;
+        padding-left: 0.3rem;
+        padding-right: 0.3rem;
+        margin-top:0.3rem
+        margin-bottom: 0.3rem;
+        border-radius: 0.5rem;
+        font-size: 0.975rem;
+        line-height:1rem;
+        font-weight : 600;
+        color: ${({ theme }) => theme.colors.gray11};
+        flex-shrink:0;
+        cursor:default;
+      }
 
+      a {
+        display: block;
+        padding: 0.25rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        margin-top: 0.25rem;
+        margin-bottom: 0.25rem;
+        border-radius: 0.75rem;
+        font-size: 0.875rem;
+        line-height: 1.25rem;
+        color: ${({ theme }) => theme.colors.gray10};
+        flex-shrink: 0;
+        cursor: pointer;
+  
         :hover {
           background-color: ${({ theme }) => theme.colors.gray4};
         }
+        &[data-active="true"] {
+          color: ${({ theme }) => theme.colors.gray12};
+          background-color: ${({ theme }) => theme.colors.gray4};
+  
+          :hover {
+            background-color: ${({ theme }) => theme.colors.gray4};
+          }
+        }
       }
     }
+    
   }
 `
